ef37d3df9bdfad3390c3fdd95870894529d182fd
konstantin <konstabarilo12@gmail.com>
2024-07-22 20:37:07 +0300

    fix: add sign in button and add link on logo

--- src/components/Header/UserInfo/UserInfo.tsx
+++ src/components/Header/UserInfo/UserInfo.tsx
@@ -2,28 +2,40 @@ import { FC } from "react";
 import { ArrowDownIcon } from "@/assets";
 import { useSelector } from "react-redux";
 import { IRootState } from "@/types";
+import { Link } from "react-router-dom";
+import { ROUTES } from "@/constants";
 
 export const UserInfo: FC = () => {
     const { username } = useSelector((state: IRootState) => state.user);
     const initials = username
-        .split(" ")
-        .map((word) => word[0])
-        .join("")
-        .toUpperCase();
+        ? username
+              .split(" ")
+              .map((word) => word[0])
+              .join("")
+              .toUpperCase()
+        : null;
 
     return (
         <div className="flex justify-between items-center">
-            <div className="flex justify-between items-center mr-[51px]">
-                <div className="w-14 h-14 rounded-[10px] bg-primary text-center py-4 mr-5">
-                    <span className="font-bold text-xl leading-6 text-white uppercase">
-                        {initials}
-                    </span>
-                </div>
+            {username ? (
+                <>
+                    <div className="flex justify-between items-center mr-[51px]">
+                        <div className="w-14 h-14 rounded-[10px] bg-primary text-center py-4 mr-5">
+                            <span className="font-bold text-xl leading-6 text-white uppercase">
+                                {initials}
+                            </span>
+                        </div>
 
-                <span className="font-semibold text-base">{username}</span>
-            </div>
+                        <span className="font-semibold text-base">{username}</span>
+                    </div>
 
-            <img src={ArrowDownIcon} className="cursor-pointer" alt="Arrow" />
+                    <img src={ArrowDownIcon} className="cursor-pointer" alt="Arrow" />
+                </>
+            ) : (
+                <Link to={ROUTES.SIGN_IN}>
+                    <span className="text-primary font-semibold text-base">Sign In</span>
+                </Link>
+            )}
         </div>
     );
 };
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,16 +2,20 @@ import { FC } from "react";
 import { LogoDarkIcon, LogoLightIcon } from "@/assets";
 import { useSelector } from "react-redux";
 import { IRootState, THEMES } from "@/types";
+import { Link } from "react-router-dom";
+import { ROUTES } from "@/constants";
 
 export const Logo: FC = () => {
     const { colorTheme } = useSelector((state: IRootState) => state.ui);
 
     return (
-        <img
-            key="Logo"
-            src={colorTheme === THEMES.LIGHT ? LogoLightIcon : LogoDarkIcon}
-            alt="Logo"
-            data-testid="Logo"
-        />
+        <Link to={ROUTES.HOME}>
+            <img
+                key="Logo"
+                src={colorTheme === THEMES.LIGHT ? LogoLightIcon : LogoDarkIcon}
+                alt="Logo"
+                data-testid="Logo"
+            />
+        </Link>
     );
 };
